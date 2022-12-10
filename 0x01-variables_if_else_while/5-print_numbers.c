b40df17c74a82a15a5197189c6a1206c0872df24
Olayimika <oluwasegunyinka.samuel@gmail.com>
2022-12-09 17:36:16 -0800

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - print numbers from 0 to 9
+*Return: Always 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+		printf("%d", x);
+
+	print("\n");
+
+	return (0);
+}
