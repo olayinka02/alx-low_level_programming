2aee980efe91a256554e0abcc53c94081eef66fb
Olayimika <oluwasegunyinka.samuel@gmail.com>
2022-12-10 17:33:09 -0800

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 48;
+	j = 48;
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
